f13f08bc9bfc76fb685bfac2e407b04a4c303d76
AdiPrawono <mrkanemi085@gmail.com>
2024-06-26 23:13:32 +0700

    first commit

---
+++ component/nav.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def navbar():
+    # Membuat 4 kolom
+    col1, col2, col3, col4 = st.columns(4, gap="small")
+
+    # Menambahkan konten di dalam kolom pertama
+    with col1:
+        st.page_link("Homepage.py", label="Home")
